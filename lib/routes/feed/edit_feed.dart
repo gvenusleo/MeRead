e0a719380089ea187168dacd11b53ace43fc5b34
GVenusLeo <gvenusleo@gmail.com>
2022-12-03 19:23:51 +0800

    feat: upgrade SetPage & EditFeedPage

--- lib/routes/feed/edit_feed.dart
+++ lib/routes/feed/edit_feed.dart
@@ -34,11 +34,11 @@ class EditFeedPageState extends State<EditFeedPage> {
           style: TextStyle(fontWeight: FontWeight.w700),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 20, 16, 20),
-        child: ListView(
-          children: [
-            TextField(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
               controller: _urlController,
               style: TextStyle(
                 color: Theme.of(context).textTheme.headlineSmall!.color,
@@ -48,8 +48,10 @@ class EditFeedPageState extends State<EditFeedPage> {
                 enabled: false,
               ),
             ),
-            const SizedBox(height: 24),
-            TextField(
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
               controller: _nameController,
               style: TextStyle(
                 color: Theme.of(context).textTheme.headlineSmall!.color,
@@ -58,8 +60,10 @@ class EditFeedPageState extends State<EditFeedPage> {
                 labelText: '订阅源名称',
               ),
             ),
-            const SizedBox(height: 24),
-            TextField(
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
               controller: _categoryController,
               style: TextStyle(
                 color: Theme.of(context).textTheme.headlineSmall!.color,
@@ -68,101 +72,97 @@ class EditFeedPageState extends State<EditFeedPage> {
                 labelText: '订阅源分类',
               ),
             ),
-            const SizedBox(height: 12),
-            const Divider(thickness: 1),
-            // TODO：获取全文
-            // ListTile(
-            //   contentPadding: const EdgeInsets.all(0),
-            //   title: const Text('是否获取全文'),
-            //   trailing: Switch(
-            //     value: widget.feed.fullText == 0 ? false : true,
-            //     onChanged: (bool value) {
-            //       setState(() {
-            //         widget.feed.fullText = value ? 1 : 0;
-            //       });
-            //     },
-            //   ),
-            // ),
-            // 文章打开方式
-            ListTile(
-              contentPadding: const EdgeInsets.all(0),
-              title: const Text('文章打开方式'),
-              trailing: DropdownButton(
-                value: widget.feed.openType,
-                elevation: 1,
-                items: [
-                  DropdownMenuItem(
-                    value: 0,
-                    child: Text(
-                      '阅读器',
-                      style: TextStyle(
-                        color: Theme.of(context).textTheme.headlineSmall!.color,
-                      ),
+          ),
+          const Divider(),
+          // TODO：获取全文
+          // ListTile(
+          //   title: const Text('是否获取全文'),
+          //   trailing: Switch(
+          //     value: widget.feed.fullText == 0 ? false : true,
+          //     onChanged: (bool value) {
+          //       setState(() {
+          //         widget.feed.fullText = value ? 1 : 0;
+          //       });
+          //     },
+          //   ),
+          // ),
+          // 文章打开方式
+          ListTile(
+            title: const Text('文章打开方式'),
+            trailing: DropdownButton(
+              value: widget.feed.openType,
+              elevation: 1,
+              items: [
+                DropdownMenuItem(
+                  value: 0,
+                  child: Text(
+                    '阅读器',
+                    style: TextStyle(
+                      color: Theme.of(context).textTheme.headlineSmall!.color,
                     ),
                   ),
-                  DropdownMenuItem(
-                    value: 1,
-                    child: Text(
-                      '内置标签页',
-                      style: TextStyle(
-                        color: Theme.of(context).textTheme.headlineSmall!.color,
-                      ),
-                    ),
-                  ),
-                  DropdownMenuItem(
-                    value: 2,
-                    child: Text(
-                      '系统浏览器',
-                      style: TextStyle(
-                        color: Theme.of(context).textTheme.headlineSmall!.color,
-                      ),
-                    ),
-                  ),
-                ],
-                onChanged: (int? value) {
-                  setState(() {
-                    widget.feed.openType = value!;
-                  });
-                },
-              ),
-            ),
-            const SizedBox(height: 24),
-            // 取消与保存
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                OutlinedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                    Navigator.pop(context);
-                  },
-                  child: const Text('取消'),
                 ),
-                const SizedBox(width: 24),
-                OutlinedButton(
-                  onPressed: () async {
-                    widget.feed.name = _nameController.text;
-                    widget.feed.category = _categoryController.text;
-                    // 如果 feed 不存在，添加 feed，否则更新 feed
-                    if (await feedExist(widget.feed.url)) {
-                      await updateFeed(widget.feed);
-                      await updatePostFeedName(
-                          widget.feed.id!, widget.feed.name);
-                      await updateFeedPostsOpenType(
-                          widget.feed.id!, widget.feed.openType);
-                    } else {
-                      await insertFeed(widget.feed);
-                    }
-
-                    if (!mounted) return;
-                    Navigator.pop(context);
-                  },
-                  child: const Text('保存'),
+                DropdownMenuItem(
+                  value: 1,
+                  child: Text(
+                    '内置标签页',
+                    style: TextStyle(
+                      color: Theme.of(context).textTheme.headlineSmall!.color,
+                    ),
+                  ),
+                ),
+                DropdownMenuItem(
+                  value: 2,
+                  child: Text(
+                    '系统浏览器',
+                    style: TextStyle(
+                      color: Theme.of(context).textTheme.headlineSmall!.color,
+                    ),
+                  ),
                 ),
               ],
+              onChanged: (int? value) {
+                setState(() {
+                  widget.feed.openType = value!;
+                });
+              },
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 24),
+          // 取消与保存
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OutlinedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+                child: const Text('取消'),
+              ),
+              const SizedBox(width: 24),
+              OutlinedButton(
+                onPressed: () async {
+                  widget.feed.name = _nameController.text;
+                  widget.feed.category = _categoryController.text;
+                  // 如果 feed 不存在，添加 feed，否则更新 feed
+                  if (await feedExist(widget.feed.url)) {
+                    await updateFeed(widget.feed);
+                    await updatePostFeedName(widget.feed.id!, widget.feed.name);
+                    await updateFeedPostsOpenType(
+                        widget.feed.id!, widget.feed.openType);
+                  } else {
+                    await insertFeed(widget.feed);
+                  }
+
+                  if (!mounted) return;
+                  Navigator.pop(context);
+                },
+                child: const Text('保存'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/routes/setting/set.dart
+++ lib/routes/setting/set.dart
@@ -1,11 +1,17 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../../routes/setting/about.dart';
-import '../../routes/setting/parse_set.dart';
-import '../../routes/setting/read_set.dart';
 import '../../states/state.dart';
+import '../../utils/key.dart';
+import '../../utils/parse.dart';
+import 'custom_css.dart';
 
 class SetPage extends StatefulWidget {
   const SetPage({super.key});
@@ -15,6 +21,46 @@ class SetPage extends StatefulWidget {
 }
 
 class _SetPageState extends State<SetPage> {
+  int fontSize = 18;
+  double lineheight = 1.5;
+  int pagePadding = 18;
+  String textAlign = 'justify';
+  bool endAddLink = false;
+  final TextEditingController _defaultCategoryController =
+      TextEditingController();
+  int defaultOpenType = 0;
+  int feedMaxSaveCount = 50;
+  bool allowDuplicate = false;
+
+  Future<void> initData() async {
+    final int size = await getFontSize();
+    final double height = await getLineheight();
+    final int padding = await getPagePadding();
+    final String align = await getTextAlign();
+    final bool link = await getEndAddLink();
+    final String defaultCategory = await getDefaultCategory();
+    final int openType = await getDefaultOpenType();
+    final int maxSaveCount = await getFeedMaxSaveCount();
+    final bool allowDup = await getAllowDuplicate();
+    setState(() {
+      fontSize = size;
+      lineheight = height;
+      pagePadding = padding;
+      textAlign = align;
+      endAddLink = link;
+      _defaultCategoryController.text = defaultCategory;
+      defaultOpenType = openType;
+      feedMaxSaveCount = maxSaveCount;
+      allowDuplicate = allowDup;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,86 +74,363 @@ class _SetPageState extends State<SetPage> {
         children: [
           // TODO：语言设置
           ListTile(
-            title: const Text(
-              '颜色主题',
+            title: const Text('颜色主题'),
+            trailing: DropdownButton(
+              value: Provider.of<ThemeModel>(context).themeIndex,
+              items: const [
+                DropdownMenuItem(
+                  value: 0,
+                  child: Text('浅色'),
+                ),
+                DropdownMenuItem(
+                  value: 1,
+                  child: Text('深色'),
+                ),
+                DropdownMenuItem(
+                  value: 2,
+                  child: Text('跟随系统'),
+                ),
+              ],
+              onChanged: (int? value) {
+                context.read<ThemeModel>().setTheme(value!);
+              },
             ),
+          ),
+          const Divider(),
+          ListTile(
+            title: const Text('字体大小'),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.remove,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  onPressed: () async {
+                    if (fontSize > 14) {
+                      setState(() {
+                        fontSize = fontSize - 1;
+                      });
+                      await setFontSize(fontSize);
+                    }
+                  },
+                ),
+                Text(fontSize.toString()),
+                IconButton(
+                  icon: Icon(
+                    Icons.add,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  onPressed: () async {
+                    if (fontSize < 24) {
+                      setState(() {
+                        fontSize = fontSize + 1;
+                      });
+                      await setFontSize(fontSize);
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            title: const Text('行间距'),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.remove,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  onPressed: () async {
+                    if (lineheight > 1.0) {
+                      setState(() {
+                        lineheight = lineheight - 0.1;
+                      });
+                      await setLineheight(lineheight);
+                    }
+                  },
+                ),
+                Text(lineheight.toStringAsFixed(1)),
+                IconButton(
+                  icon: Icon(
+                    Icons.add,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  onPressed: () async {
+                    if (lineheight < 3.0) {
+                      setState(() {
+                        lineheight = lineheight + 0.1;
+                      });
+                      await setLineheight(lineheight);
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            title: const Text('页面边距'),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.remove,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  onPressed: () async {
+                    if (pagePadding > 0) {
+                      setState(() {
+                        pagePadding = pagePadding - 2;
+                      });
+                      await setPagePadding(pagePadding);
+                    }
+                  },
+                ),
+                Text(pagePadding.toString()),
+                IconButton(
+                  icon: Icon(
+                    Icons.add,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  onPressed: () async {
+                    if (pagePadding < 36) {
+                      setState(() {
+                        pagePadding = pagePadding + 2;
+                      });
+                      await setPagePadding(pagePadding);
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            title: const Text('文字对齐'),
+            trailing: DropdownButton(
+              value: textAlign,
+              items: [
+                DropdownMenuItem(
+                  value: 'left',
+                  child: Text(
+                    '左对齐',
+                    style: TextStyle(
+                      color: Theme.of(context).textTheme.headlineSmall!.color,
+                    ),
+                  ),
+                ),
+                DropdownMenuItem(
+                  value: 'right',
+                  child: Text(
+                    '右对齐',
+                    style: TextStyle(
+                      color: Theme.of(context).textTheme.headlineSmall!.color,
+                    ),
+                  ),
+                ),
+                DropdownMenuItem(
+                  value: 'justify',
+                  child: Text(
+                    '两端对齐',
+                    style: TextStyle(
+                      color: Theme.of(context).textTheme.headlineSmall!.color,
+                    ),
+                  ),
+                ),
+              ],
+              onChanged: (value) async {
+                setState(() {
+                  textAlign = value!;
+                });
+                await setTextAlign(value!);
+              },
+            ),
+          ),
+          ListTile(
+            title: const Text('文章末尾显示原文链接'),
+            trailing: Switch(
+              value: endAddLink,
+              onChanged: (bool value) async {
+                setState(() {
+                  endAddLink = value;
+                });
+                await setEndAddLink(value);
+              },
+            ),
+          ),
+          ListTile(
+            title: const Text('自定义 CSS'),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () {
+              Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  builder: (context) => const CustomCssPage(),
+                ),
+              );
+            },
+          ),
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextFormField(
+              controller: _defaultCategoryController,
+              style: TextStyle(
+                color: Theme.of(context).textTheme.headlineSmall!.color,
+              ),
+              decoration: const InputDecoration(
+                labelText: '订阅源默认分类名',
+              ),
+              onFieldSubmitted: (String value) async {
+                await setDefalutCategory(value);
+              },
+            ),
+          ),
+          ListTile(
+            title: const Text('文章默认打开方式'),
+            trailing: DropdownButton(
+              value: defaultOpenType,
+              items: const [
+                DropdownMenuItem(
+                  value: 0,
+                  child: Text('阅读器'),
+                ),
+                DropdownMenuItem(
+                  value: 1,
+                  child: Text('内置标签页'),
+                ),
+                DropdownMenuItem(
+                  value: 2,
+                  child: Text('系统浏览器'),
+                ),
+              ],
+              onChanged: (int? value) async {
+                setState(() {
+                  defaultOpenType = value!;
+                });
+                await setDefaultOpenType(value!);
+              },
+            ),
+          ),
+          ListTile(
+            title: const Text('每个订阅源最大保存数'),
+            trailing: DropdownButton<int>(
+              value: feedMaxSaveCount,
+              items: [
+                DropdownMenuItem(
+                  value: 50,
+                  child: Text(
+                    '50',
+                    style: TextStyle(
+                      color: Theme.of(context).textTheme.headlineSmall!.color,
+                    ),
+                  ),
+                ),
+                DropdownMenuItem(
+                  value: 100,
+                  child: Text(
+                    '100',
+                    style: TextStyle(
+                      color: Theme.of(context).textTheme.headlineSmall!.color,
+                    ),
+                  ),
+                ),
+                DropdownMenuItem(
+                  value: 300,
+                  child: Text(
+                    '300',
+                    style: TextStyle(
+                      color: Theme.of(context).textTheme.headlineSmall!.color,
+                    ),
+                  ),
+                ),
+                DropdownMenuItem(
+                  value: 500,
+                  child: Text(
+                    '500',
+                    style: TextStyle(
+                      color: Theme.of(context).textTheme.headlineSmall!.color,
+                    ),
+                  ),
+                ),
+              ],
+              onChanged: (int? value) async {
+                if (value != null) {
+                  setState(() {
+                    feedMaxSaveCount = value;
+                  });
+                  await setFeedMaxSaveCount(value);
+                }
+              },
+            ),
+          ),
+          ListTile(
+            title: const Text('允许文章重复'),
+            trailing: Switch(
+              value: allowDuplicate,
+              onChanged: (bool value) async {
+                setState(() {
+                  allowDuplicate = value;
+                });
+                await setAllowDuplicate(value);
+              },
+            ),
+          ),
+          const Divider(),
+          ListTile(
+            title: const Text('导入 OPML'),
             trailing: const Icon(Icons.chevron_right),
             onTap: () async {
-              showModalBottomSheet(
-                context: context,
-                builder: (BuildContext context) {
-                  return SizedBox(
-                    height: 200,
-                    child: Column(
-                      children: [
-                        RadioListTile(
-                          value: 0,
-                          groupValue:
-                              Provider.of<ThemeModel>(context).themeIndex,
-                          title: const Text('浅色'),
-                          onChanged: (int? value) async {
-                            context.read<ThemeModel>().setTheme(value!);
-                            if (!mounted) return;
-                            Navigator.pop(context);
-                          },
-                        ),
-                        RadioListTile(
-                          value: 1,
-                          groupValue:
-                              Provider.of<ThemeModel>(context).themeIndex,
-                          title: const Text('深色'),
-                          onChanged: (value) {
-                            context.read<ThemeModel>().setTheme(value!);
-                            if (!mounted) return;
-                            Navigator.pop(context);
-                          },
-                        ),
-                        RadioListTile(
-                          value: 2,
-                          groupValue:
-                              Provider.of<ThemeModel>(context).themeIndex,
-                          title: const Text('跟随系统'),
-                          onChanged: (value) {
-                            context.read<ThemeModel>().setTheme(value!);
-                            if (!mounted) return;
-                            Navigator.pop(context);
-                          },
-                        ),
-                      ],
+              // 打开文件选择器
+              final result = await FilePicker.platform.pickFiles(
+                type: FileType.custom,
+                allowedExtensions: ['opml', 'xml'],
+              );
+              if (result != null) {
+                if (!mounted) return;
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                      '开始后台导入',
+                      textAlign: TextAlign.center,
                     ),
-                  );
-                },
-              );
+                    duration: Duration(seconds: 1),
+                  ),
+                );
+                final int failCount = await parseOpml(result);
+                if (!mounted) return;
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      failCount == 0 ? '导入成功' : '$failCount 个订阅源导入失败',
+                      textAlign: TextAlign.center,
+                    ),
+                    duration: const Duration(seconds: 1),
+                  ),
+                );
+              }
             },
           ),
           ListTile(
-            title: const Text(
-              '阅读页面',
-            ),
+            title: const Text('导出 OPML'),
             trailing: const Icon(Icons.chevron_right),
-            onTap: () {
-              Navigator.push(
-                context,
-                CupertinoPageRoute(
-                  builder: (context) => const ReadSetPage(),
-                ),
+            onTap: () async {
+              String opmlStr = await exportOpml();
+              // opmlStr 字符串写入 feeds.opml 文件并分享，分享后删除文件
+              final Directory tempDir = await getTemporaryDirectory();
+              final File file = File('${tempDir.path}/feeds-from-MeReader.xml');
+              await file.writeAsString(opmlStr);
+              await Share.shareXFiles(
+                [XFile(file.path)],
+                text: '分享 OPML 文件',
               );
+              await file.delete();
             },
           ),
-          ListTile(
-            title: const Text(
-              '解析设置',
-            ),
-            trailing: const Icon(Icons.chevron_right),
-            onTap: () {
-              Navigator.push(
-                context,
-                CupertinoPageRoute(
-                  builder: (context) => const ParseSetPage(),
-                ),
-              );
-            },
-          ),
+          const Divider(),
           ListTile(
             title: const Text(
               '关于应用',
