ec0dcdd1eeb15e24398e0372a9b81d9d1842dd69
GVenusLeo <gvenusleo@gmail.com>
2022-12-07 18:24:52 +0800

    release: v0.2.1

--- lib/routes/setting/about.dart
+++ lib/routes/setting/about.dart
@@ -33,7 +33,7 @@ class AboutPage extends StatelessWidget {
             const SizedBox(height: 8),
             const Center(
               child: Text(
-                'V 0.2.0',
+                'V 0.2.1',
                 style: TextStyle(fontSize: 18),
               ),
             ),
