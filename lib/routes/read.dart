ca5ba59ccf28653cf021327b5800bc90c4c58235
liuyuxin <gvenusleo@gmail.com>
2023-09-01 14:54:17 +0800

    revert to webview

--- lib/routes/read.dart
+++ lib/routes/read.dart
@@ -1,14 +1,13 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_html/flutter_html.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:html/parser.dart' as html_parser;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:html_main_element/html_main_element.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:meread/models/post.dart';
 import 'package:meread/provider/read_page_provider.dart';
+import 'package:meread/provider/theme_provider.dart';
 import 'package:meread/utils/open_url_util.dart';
 import 'package:provider/provider.dart';
 import 'package:share_plus/share_plus.dart';
@@ -32,6 +31,9 @@ class ReadPageState extends State<ReadPage> {
   // 内容 html
   String contentHtml = '';
 
+  late InAppWebViewController webViewController;
+  GlobalKey<ScaffoldState> webViewKey = GlobalKey<ScaffoldState>();
+
   @override
   void initState() {
     super.initState();
@@ -40,113 +42,196 @@ class ReadPageState extends State<ReadPage> {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    final String textColor = themeData.textTheme.bodyLarge!.color!.value
+        .toRadixString(16)
+        .substring(2);
+    final String backgroundColor =
+        themeData.scaffoldBackgroundColor.value.toRadixString(16).substring(2);
     final titleStr = '<h1>${widget.post.title}</h1>';
+    final String cssStr = '''
+@font-face {
+  font-family: 'customFont';
+  src: url('${widget.fontDir}/${context.watch<ThemeProvider>().themeFont}');
+}
+body {
+  font-family: 'customFont';
+  font-size: ${context.watch<ReadPageProvider>().fontSize}px;
+  line-height: ${context.watch<ReadPageProvider>().lineHeight};
+  color: #$textColor;
+  background-color: #$backgroundColor;
+  width: auto;
+  height: auto;
+  margin: 0;
+  word-wrap: break-word;
+  padding: 12px ${context.watch<ReadPageProvider>().pagePadding}px !important;
+  text-align: ${context.watch<ReadPageProvider>().textAlign};
+}
+h1 {
+  font-size: 1.5em;
+  font-weight: 700;
+}
+h2 {
+  font-size: 1.25em;
+  font-weight: 700;
+}
+h3,h4,h5,h6 {
+  font-size: 1.0em;
+  font-weight: 700;
+}
+img,figure,video,iframe {
+  max-width: 100% !important;
+  height: auto;
+  margin: 0 auto;
+  display: block;
+}
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.post.feedName),
-        actions: [
-          /* 在浏览器中打开 */
-          IconButton(
-            onPressed: () {
-              openUrl(widget.post.link);
-            },
-            icon: const Icon(Icons.open_in_browser_outlined),
+a {
+  color: #$textColor;
+  text-decoration: none;
+  border-bottom: 1px solid #$textColor;
+  padding-bottom: 1px;
+  word-break: break-all;
+}
+blockquote {
+  margin: 0;
+  padding: 0 0 0 16px;
+  border-left: 4px solid #9e9e9e;
+}
+pre {
+  white-space: pre-wrap;
+  word-break: break-all;
+}
+table {
+  width: 100% !important;
+  table-layout: fixed;
+}
+table td {
+  padding: 0 8px;
+}
+
+table, th, td {
+  border: 1px solid #$textColor;
+  border-collapse: collapse;
+}
+${context.watch<ReadPageProvider>().customCss}
+''';
+
+    return WillPopScope(
+      onWillPop: () async {
+        if (await webViewController.canGoBack()) {
+          await webViewController.goBack();
+          return false;
+        } else {
+          return true;
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.post.feedName),
+          actions: [
+            /* 在浏览器中打开 */
+            IconButton(
+              onPressed: () {
+                openUrl(widget.post.link);
+              },
+              icon: const Icon(Icons.open_in_browser_outlined),
+            ),
+            /* 获取全文 */
+            IconButton(
+              onPressed: getFullText,
+              icon: const Icon(Icons.article_outlined),
+            ),
+            PopupMenuButton(
+              position: PopupMenuPosition.under,
+              itemBuilder: (BuildContext context) {
+                return <PopupMenuEntry>[
+                  /* 标记为未读 */
+                  PopupMenuItem(
+                    onTap: () async {
+                      await widget.post.markAsUnread();
+                    },
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(Icons.visibility_off_outlined, size: 20),
+                        const SizedBox(width: 10),
+                        Text(
+                          AppLocalizations.of(context)!.markAsUnread,
+                        ),
+                      ],
+                    ),
+                  ),
+                  /* 更改收藏状态 */
+                  PopupMenuItem(
+                    onTap: () async {
+                      await widget.post.changeFavorite();
+                    },
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(Icons.bookmark_border_outlined, size: 20),
+                        const SizedBox(width: 10),
+                        Text(
+                          widget.post.favorite
+                              ? AppLocalizations.of(context)!.cancelCollect
+                              : AppLocalizations.of(context)!.collectPost,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const PopupMenuDivider(),
+                  /* 复制链接 */
+                  PopupMenuItem(
+                    onTap: () {
+                      Clipboard.setData(ClipboardData(text: widget.post.link));
+                    },
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(Icons.link_outlined, size: 20),
+                        const SizedBox(width: 10),
+                        Text(
+                          AppLocalizations.of(context)!.copyLink,
+                        ),
+                      ],
+                    ),
+                  ),
+                  /* 分享 */
+                  PopupMenuItem(
+                    onTap: () {
+                      Share.share(
+                        widget.post.link,
+                        subject: widget.post.title,
+                      );
+                    },
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(Icons.share_outlined, size: 20),
+                        const SizedBox(width: 10),
+                        Text(
+                          AppLocalizations.of(context)!.sharePost,
+                        ),
+                      ],
+                    ),
+                  ),
+                ];
+              },
+            ),
+          ],
+        ),
+        body: SafeArea(
+          child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 800),
+            child: _buildBody(titleStr, cssStr),
           ),
-          /* 获取全文 */
-          IconButton(
-            onPressed: getFullText,
-            icon: const Icon(Icons.article_outlined),
-          ),
-          PopupMenuButton(
-            position: PopupMenuPosition.under,
-            itemBuilder: (BuildContext context) {
-              return <PopupMenuEntry>[
-                /* 标记为未读 */
-                PopupMenuItem(
-                  onTap: () async {
-                    await widget.post.markAsUnread();
-                  },
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Icon(Icons.visibility_off_outlined, size: 20),
-                      const SizedBox(width: 10),
-                      Text(
-                        AppLocalizations.of(context)!.markAsUnread,
-                      ),
-                    ],
-                  ),
-                ),
-                /* 更改收藏状态 */
-                PopupMenuItem(
-                  onTap: () async {
-                    await widget.post.changeFavorite();
-                  },
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Icon(Icons.bookmark_border_outlined, size: 20),
-                      const SizedBox(width: 10),
-                      Text(
-                        widget.post.favorite
-                            ? AppLocalizations.of(context)!.cancelCollect
-                            : AppLocalizations.of(context)!.collectPost,
-                      ),
-                    ],
-                  ),
-                ),
-                const PopupMenuDivider(),
-                /* 复制链接 */
-                PopupMenuItem(
-                  onTap: () {
-                    Clipboard.setData(ClipboardData(text: widget.post.link));
-                  },
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Icon(Icons.link_outlined, size: 20),
-                      const SizedBox(width: 10),
-                      Text(
-                        AppLocalizations.of(context)!.copyLink,
-                      ),
-                    ],
-                  ),
-                ),
-                /* 分享 */
-                PopupMenuItem(
-                  onTap: () {
-                    Share.share(
-                      widget.post.link,
-                      subject: widget.post.title,
-                    );
-                  },
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Icon(Icons.share_outlined, size: 20),
-                      const SizedBox(width: 10),
-                      Text(
-                        AppLocalizations.of(context)!.sharePost,
-                      ),
-                    ],
-                  ),
-                ),
-              ];
-            },
-          ),
-        ],
-      ),
-      body: SafeArea(
-        child: AnimatedSwitcher(
-          duration: const Duration(milliseconds: 800),
-          child: _buildBody(titleStr),
         ),
       ),
     );
   }
 
-  Widget _buildBody(String titleStr) {
+  Widget _buildBody(String titleStr, String cssStr) {
     if (_index == 0) {
       return Center(
         child: SizedBox(
@@ -165,140 +250,36 @@ class ReadPageState extends State<ReadPage> {
       );
     }
     String html = '''<!DOCTYPE html>
-<html>
+ <html>
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <style>
-${context.watch<ReadPageProvider>().customCss}
+$cssStr
 </style>
 </head>
 <body>
 $titleStr
 $contentHtml
 </body>
+</html>
 ''';
-    return SingleChildScrollView(
-      padding: EdgeInsets.symmetric(
-        horizontal: context.watch<ReadPageProvider>().pagePadding.toDouble(),
+    return InAppWebView(
+      initialData: InAppWebViewInitialData(
+        data: html,
+        baseUrl: Uri.directory(widget.fontDir),
       ),
-      child: SelectionArea(
-        child: Html(
-          data: html,
-          style: {
-            'body': Style(
-              fontSize: FontSize(
-                context.watch<ReadPageProvider>().fontSize.toDouble(),
-              ),
-              lineHeight: LineHeight(
-                context.watch<ReadPageProvider>().lineHeight,
-              ),
-              textAlign: {
-                    'left': TextAlign.left,
-                    'right': TextAlign.right,
-                    'center': TextAlign.center,
-                    'justify': TextAlign.justify,
-                  }[context.watch<ReadPageProvider>().textAlign] ??
-                  TextAlign.justify,
-            ),
-            'h1': Style(
-              fontSize: FontSize(
-                context.watch<ReadPageProvider>().fontSize.toDouble() * 1.5,
-              ),
-            ),
-            'h2': Style(
-              fontSize: FontSize(
-                context.watch<ReadPageProvider>().fontSize.toDouble() * 1.3,
-              ),
-            ),
-            'h3': Style(
-              fontSize: FontSize(
-                context.watch<ReadPageProvider>().fontSize.toDouble() * 1.1,
-              ),
-            ),
-            'h4': Style(
-              fontSize: FontSize(
-                context.watch<ReadPageProvider>().fontSize.toDouble(),
-              ),
-            ),
-            'h5': Style(
-              fontSize: FontSize(
-                context.watch<ReadPageProvider>().fontSize.toDouble(),
-              ),
-            ),
-            'h6': Style(
-              fontSize: FontSize(
-                context.watch<ReadPageProvider>().fontSize.toDouble(),
-              ),
-            ),
-          },
-          onLinkTap: (url, attributes, element) {
-            if (url != null) {
-              openUrl(url);
-            }
-          },
-          extensions: [
-            OnImageTapExtension(
-              onImageTap: (url, attributes, element) {
-                if (url != null) {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        icon: const Icon(Icons.image_outlined),
-                        title: Text(AppLocalizations.of(context)!.saveImage),
-                        content: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.network(url),
-                            const SizedBox(height: 12),
-                            Text(AppLocalizations.of(context)!.saveImageInfo),
-                          ],
-                        ),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: Text(AppLocalizations.of(context)!.cancel),
-                          ),
-                          TextButton(
-                            onPressed: () async {
-                              var response = await Dio().get(
-                                url,
-                                options: Options(
-                                  responseType: ResponseType.bytes,
-                                ),
-                              );
-                              final result = await ImageGallerySaver.saveImage(
-                                Uint8List.fromList(response.data),
-                                quality: 80,
-                                name: attributes['alt'] ?? 'image',
-                              );
-                              if (result['isSuccess']) {
-                                if (!mounted) return;
-                                Fluttertoast.showToast(
-                                  msg: AppLocalizations.of(context)!
-                                      .saveImageSuccess,
-                                );
-                              }
-                              if (!mounted) return;
-                              Navigator.of(context).pop();
-                            },
-                            child: Text(AppLocalizations.of(context)!.ok),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                }
-              },
-            ),
-          ],
+      initialOptions: InAppWebViewGroupOptions(
+        android: AndroidInAppWebViewOptions(
+          useHybridComposition: true,
+        ),
+        crossPlatform: InAppWebViewOptions(
+          transparentBackground: true,
         ),
       ),
+      onWebViewCreated: (controller) {
+        webViewController = controller;
+      },
     );
   }
 
