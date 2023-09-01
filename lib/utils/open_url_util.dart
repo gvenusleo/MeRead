c5badcd19a1af9f652cb73af9d40dddfcebbf7fb
liuyuxin <gvenusleo@gmail.com>
2023-09-01 15:34:32 +0800

    Update open_url_util.dart

--- lib/utils/open_url_util.dart
+++ lib/utils/open_url_util.dart
@@ -4,12 +4,7 @@ import 'package:url_launcher/url_launcher.dart';
 /// 使用 [InAppBrowser] 打开一个 url
 void openUrl(String url) {
   try {
-    InAppBrowser browser = InAppBrowser();
-    browser.openUrlRequest(
-      urlRequest: URLRequest(
-        url: Uri.parse(url),
-      ),
-    );
+    ChromeSafariBrowser().open(url: Uri.parse(url));
   } catch (e) {
     launchUrl(
       Uri.parse(url),
