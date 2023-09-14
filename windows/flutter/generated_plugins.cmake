98efc2e79820bb4432fd724e7a941e460bc5b4a4
liuyuxin <gvenusleo@gmail.com>
2023-09-14 14:36:13 +0800

    change maven URL && add window_manager

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,8 +5,10 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   dynamic_color
   isar_flutter_libs
+  screen_retriever
   share_plus
   url_launcher_windows
+  window_manager
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
